a964ff902c8cf854d06cee9f94dc2e51f50227c5
Lawrence <jack88634123@gmail.com>
2025-06-26 10:57:53 +0800

    add hello.java
    hi

---
+++ abc.java
@@ -0,0 +1,9 @@
+public class abc {
+    
+    public static void main(String[] var0) {
+       byte var1 = 5;
+       System.out.println("I have " + var1 + " books");
+       System.out.println("You have " + var1 + " books, too");
+    }
+ }
+ 
