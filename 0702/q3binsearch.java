e267aa01cdade9d9454dc5ac5b3aa88b30e309a8
Lawrence <98686152+YUN0019@users.noreply.github.com>
2025-07-02 13:30:22 +0800

    Update q3binsearch.java

--- 0702/q3binsearch.java
+++ 0702/q3binsearch.java
@@ -1,31 +1,38 @@
 
 import java.util.Scanner;
+
 public class q3binsearch {
-    public  static int binarySearch(int[] n , int key){
+    // 二元搜尋演算法 架構
+    public static int binarySearch(int[] arr, int key) {
+    int left = 0;
+    int right = arr.length - 1;
+    int count =0;
 
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        count++;
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+        
     }
-
-
-
+    System.out.println(count);  // 這個位置是不管怎樣都要印出count 次數
+    return -1;
+}
     public static void main(String[] args) {
-        int []n = {};
-    }
-
-
-
-
-
-
-
-
-
-
-    public static void main(String[] args) {
-        System.out.println("請輸入key");
+        System.out.println("請輸入key值：");
         Scanner target = new Scanner(System.in);
         int key = target.nextInt();
-        int[]n = {2,4,6,8,10,12,14,16};
-        
+        int []arr = {2,4,6,8,10,12,14,16};
+        int result = binarySearch(arr, key);
+        System.out.println(result);
         
     }
-}
+  
+
+ }
+
