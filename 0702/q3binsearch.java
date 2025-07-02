378d9f6ff543288e49967d7a7c15374ae0bad1a8
Lawrence <98686152+YUN0019@users.noreply.github.com>
2025-07-02 12:20:21 +0800

    Create q3binsearch.java

---
+++ 0702/q3binsearch.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class q3binsearch {
+    public  static int binarySearch(int[] n , int key){
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int []n = {};
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("請輸入key");
+        Scanner target = new Scanner(System.in);
+        int key = target.nextInt();
+        int[]n = {2,4,6,8,10,12,14,16};
+        
+        
+    }
+}
