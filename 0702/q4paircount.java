78cf156e556f00131bd89dafc376528cc1fbfcf6
Lawrence <98686152+YUN0019@users.noreply.github.com>
2025-07-02 14:07:22 +0800

    Create q4paircount.java

---
+++ 0702/q4paircount.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class q4paircount {
+    public static void main(String[] args) {
+        System.out.println("輸入n ：");
+        Scanner target = new Scanner(System.in);
+        int n = target.nextInt();
+        
+        int []arr = new int[n];
+        fot(int i =0; i<n)
+    }
+}
